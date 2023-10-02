7e164cab2d0b0af8813b01efe5350eba49135c50
hgkarremans <hg.karremans@student.avans.nl>
2023-10-02 13:35:55 +0200

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VoedselVerspilling.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
